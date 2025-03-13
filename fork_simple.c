daafb62f2052d7beb0c7a33fe1b1c7f143a9f6da
decapix <decapixd@gmail.com>
2025-03-13 15:36:03 +0100

    better

--- fork_simple.c
+++ fork_simple.c
@@ -1,37 +1,41 @@
 
 #include "pipex.h"
 
+static void duppp(t_pipexelement *head, int *fd, int in_fd)
+{
+    if (head->fd_in == -228)
+        dup2(in_fd, STDIN_FILENO);
+    else
+        dup2(head->fd_in, STDIN_FILENO);
+    if (head->fd_out == -228)
+        dup2(fd[1], STDOUT_FILENO);
+    else
+        dup2(head->fd_out, STDOUT_FILENO);
+    close(fd[0]);
+}
 
 int make_process(t_pipexelement *head, char **v, char **env)
 {
-    int fd[2], in_fd = 0;
+    int fd[2];
+    int in_fd;
+    char *st;
+    char **stt;
 
+    in_fd = 0;
     while (head != NULL) {
         pipe(fd);
         if ((head->pid_fork = fork()) == 0) {
-
-            if (head->fd_in == -228)
-                dup2(in_fd, STDIN_FILENO);  // Utiliser l'ancien in_fd comme entrée
-            else
-                dup2(head->fd_in, STDIN_FILENO);
-            if (head->fd_out == -228)   // S'il y a une commande suivante, utiliser fd[1] comme sortie
-                dup2(fd[1], STDOUT_FILENO);
-            else
-                dup2(head->fd_out, STDOUT_FILENO);
-            close(fd[0]); // Fermer l'entrée du pipe dans le processus enfant
-            execve(find_binary(head->cmd, env), ft_split(head->cmd, ' '), env); // Assurez-vous de parser la commande correctement
-            perror("execvp");
-            exit(EXIT_FAILURE);
+            duppp(head, fd, in_fd);
+            st = find_binary(head->cmd, env);
+            stt = ft_split(head->cmd, ' ');
+            execve(st, stt, env);
         }
-        close(fd[1]);  // Fermer la sortie du pipe dans le parent
-        if (in_fd != 0) {
-            close(in_fd);  // Fermer le précédent in_fd s'il n'est pas le stdin initial
-        }
-        in_fd = fd[0];  // Préparer in_fd pour la prochaine commande
+        close(fd[1]);
+        if (in_fd != 0)
+            close(in_fd);
+        in_fd = fd[0];
         head = head->next;
     }
-
-    // Fermer le dernier in_fd
     if (in_fd != 0)
         close(in_fd);
     return (0);
--- main_simple.c
+++ main_simple.c
@@ -3,88 +3,6 @@
 #include <sys/ucontext.h>
 #include <unistd.h>
 
-// static t_pipexelement	*init_fork_1(t_pipexelement *pipexobj, char **v)
-// {
-// 	pipexobj->fd_file1 = open(v[1], O_RDONLY);
-// 	if (pipexobj->fd_file1 == -1)
-// 	{
-// 		pipexobj->error = "open";
-// 		return (pipexobj);
-// 	}
-// 	if (pipe(pipexobj->pip_fd) == -1)
-// 	{
-// 		perror("pipe");
-// 		exit(EXIT_FAILURE);
-// 	}
-// 	pipexobj->pid_fork1 = fork();
-// 	if (pipexobj->pid_fork1 == -1)
-// 	{
-// 		perror("fork");
-// 		exit(EXIT_FAILURE);
-// 	}
-// 	return (pipexobj);
-// }
-
-
-
-
-// static t_pipexelement	*childrens(t_pipexelement *pipexobj, char **v,
-// 		char **env)
-// {
-// 	int    status;
-
-// 	pipexobj->pid_fork2 = fork();
-// 	if (pipexobj->pid_fork2 == -1)
-// 	{
-// 		perror("fork");
-// 		exit(EXIT_FAILURE);
-// 	}
-// 	if (pipexobj->pid_fork2 == 0)
-// 	{
-//         pipexobj->fd_file2 = open(v[4], O_WRONLY | O_CREAT, 0644);
-//        	if (pipexobj->fd_file2 == -1)
-//        	{
-//       		pipexobj->error = "open";
-//       		return (pipexobj);
-//        	}
-//         FdSet fds1 = {pipexobj->pip_fd[0], pipexobj->fd_file2, pipexobj->pip_fd[1]};
-//         if (ft_strncmp(childs(fds1, v[3], env), "ok", 2) != 0) {
-//             return (pipexobj);
-//         }
-// 	}
-// 	else
-// 	{
-// 	    waitpid(pipexobj->pid_fork1, &status, 0);
-// 		waitpid(pipexobj->pid_fork2, &status, 0);
-// 		close(pipexobj->fd_file2);
-// 		close(pipexobj->fd_file1);
-// 		close(pipexobj->pip_fd[0]);
-// 		close(pipexobj->pip_fd[1]);
-// 	}
-// 	return (pipexobj);
-// }
-//
-
-
-// static char *childs(FdSet fds, char *cmd, char **env) {
-//     if (dup2(fds.fd_in, STDIN_FILENO) == -1) {
-//         perror("dup2");
-//         close(fds.fd_in);
-//         exit(EXIT_FAILURE);
-//     }
-//     if (dup2(fds.fd_out, STDOUT_FILENO) == -1) {
-//         perror("dup2");
-//         close(fds.fd_out);
-//         exit(EXIT_FAILURE);
-//     }
-//     close(fds.fd_extra);
-//     if (execve(find_binary(cmd, env), ft_split(cmd, ' '), NULL) == -1) {
-//         perror("execve");
-//         exit(EXIT_FAILURE);
-//     }
-//     return "ok";
-// }
-
 
 static void filesfds(int *fd_files, char **v, int c)
 {
@@ -96,7 +14,6 @@ static void filesfds(int *fd_files, char **v, int c)
         exit(EXIT_FAILURE);
     }
 }
-
 
 
 int put_fds(t_pipexelement *first, int *fd_files)
@@ -132,18 +49,6 @@ t_pipexelement *init_chain(t_pipexelement	*pipexobj, char *vi)
     return (pipexobj);
 }
 
-void print_pipex_list(t_pipexelement *head) {
-    t_pipexelement *current = head;
-    int position = 0;
-
-    while (current != NULL)
-    {
-        printf("Position: %d, Command: %s, fd1 : %d, fd2 : %d\n", position, current->cmd, current->fd_in, current->fd_out);
-        current = current->next;
-        position++;
-    }
-}
-
 
 int	main(int c, char **v, char **env)
 {
@@ -170,7 +75,7 @@ int	main(int c, char **v, char **env)
     }
 
 	if (put_fds(first, fd_files) == 1)
-	   return (127);
+	   return (-1);
 	if (make_process(first, v, env) == 1)
 	   return (127);
 	return (clean_exit(first));
